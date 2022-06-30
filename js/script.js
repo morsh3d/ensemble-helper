14282e7e42d0e47e463a49fac8304988afa83e6b
Yasmin Morshed <snowmoss10@gmail.com>
2022-06-30 18:16:51 +0100

    Working! :D

--- js/script.js
+++ js/script.js
@@ -1,119 +1,67 @@
-class EnsembleTimer {
-  /**
-   * @constructor
-   */
-  constructor() {
-    /**
-     * @type {HTMLElement}
-     */
-    this.ensembleBtn = document.querySelector("#ensemble-btn");
-    /**
-     * @type {HTMLElement}
-     */
-    this.longBreakBtn = document.querySelector("#long-break-btn");
-    /**
-     * @type {HTMLElement}
-     */
-    this.shortBreakBtn = document.querySelector("#short-break-btn");
-    /**
-     * @type {HTMLElement}
-     */
-    this.timeDisplay = document.querySelector("#time-display");
-    /**
-     * @type {null | setInterval}
-     */
-    this.timeout = null;
-  }
-  setEventListeners() {
-    /**
-     * @type {this}
-     */
-    let iself = this;
-    /**
-     * @type {HTMLElement}
-     */
-    this.longBreakBtn.addEventListener("click", function () {
-      iself.startTimer(15);
-    });
-    /**
-     * @type {HTMLElement}
-     */
-    this.shortBreakBtn.addEventListener("click", function () {
-      iself.startTimer(5);
-    });
-    /**
-     * @type {HTMLElement}
-     */
-  }
-
-  /**
-   * @param seconds
-   * @returns {string|*}
-   */
-  timePadding = (seconds) => {
-    return seconds < 10 ? `0${seconds.toString()}` : seconds;
-  };
-  /**
-   * @param seconds
-   */
-  renderTime = (seconds) => {
-    const minutesInText = Math.floor(seconds / 60);
-    const actualSeconds = seconds % 60;
-
-    const secondsInText = this.timePadding(actualSeconds);
-    const time = `${minutesInText}:${secondsInText}`;
-
-    this.timeDisplay.textContent = time;
-    document.title = `meow ${time}`;
-  };
-  /**
-   * @param timeInMinutes
-   */
-  startTimer = (timeInMinutes) => {
-    let iself = this;
-    clearInterval(this.timeout);
-    let ensembleTimeInSeconds = timeInMinutes * 60;
-    this.renderTime(ensembleTimeInSeconds);
-    this.timeout = setInterval(function () {
-      if (ensembleTimeInSeconds > 0) {
-        ensembleTimeInSeconds--;
-      }
-      iself.renderTime(ensembleTimeInSeconds);
-    }, 1000);
-  };
-}
-
-/**
- * @type {EnsembleTimer}
- */
-let Timer = new EnsembleTimer();
-Timer.setEventListeners();
-
 // Local Storage functions
 function getPeople() {
-  return localStorage.getItem("people")?.split(",") ?? [];
+  return (
+    localStorage
+      .getItem("people")
+      ?.split(",")
+      ?.filter((x) => x) ?? []
+  );
 }
 
 function addPerson(name) {
   localStorage.setItem(
     "people",
-    [localStorage.getItem("people"), text].filter((x) => x).join(",")
+    [localStorage.getItem("people"), name].filter((x) => x).join(",")
   );
 }
 
 function removeAllPeople() {
-  localStorage.setItem("people", null);
+  localStorage.setItem("people", "");
+}
+
+function setStartTime(date) {
+  localStorage.setItem("start_time", date ? date.toISOString() : "");
+}
+
+function getStartTime() {
+  const dateString = localStorage.getItem("start_time");
+  return dateString ? new Date(dateString) : null;
 }
 
 // UI functions
+function padStartWithZero(value) {
+  let valueString = value.toString();
+  if (valueString.length === 1) {
+    valueString = `0${valueString}`;
+  }
+
+  return valueString;
+}
+function getDisplayTime() {
+  const startTime = getStartTime();
+  if (!startTime) {
+    return null;
+  }
+
+  const time = new Date(startTime.getTime() + 10 * 60000);
+  let msec = time - new Date();
+  const hh = Math.floor(msec / 1000 / 60 / 60);
+  msec -= hh * 1000 * 60 * 60;
+  const mm = Math.floor(msec / 1000 / 60);
+  msec -= mm * 1000 * 60;
+  const ss = Math.floor(msec / 1000);
+  msec -= ss * 1000;
+
+  return `${padStartWithZero(mm)}:${padStartWithZero(ss)}`;
+}
+
 function setText(id, text) {
   document.getElementById(id).innerText = text;
 }
 
 function renderPerson(name) {
   const node = document.createElement("Li");
-  const text = document.getElementById("user_input").value;
-  const textnode = document.createTextNode(text);
+  const textnode = document.createTextNode(name);
   node.appendChild(textnode);
   document.getElementById("list_item").appendChild(node);
 }
@@ -122,9 +70,29 @@ function renderPeopleList() {
   document.getElementById("list_item").innerHTML = "";
   const people = getPeople();
 
-  for (person in people) {
-    renderPerson(person);
+  for (let i = 0; i < people.length; i++) {
+    renderPerson(people[i]);
   }
+}
+
+function clearEnsembleList() {
+  document.getElementById("roles").style.display = "none";
+}
+
+function renderEnsembleList() {
+  const startTime = getStartTime();
+  if (!startTime) {
+    return;
+  }
+
+  const people = getPeople();
+  const list = document.getElementById("list_item").getElementsByTagName("li");
+
+  document.getElementById("roles").style.display = "block";
+  setText("roles-title", "Roles");
+  setText("role1", people[0] + " " + "is NAVIGATOR");
+  setText("role2", people[1] + " " + "is TYPIST");
+  setText("role3", people[2] + " " + "is CO-NAVIGATOR");
 }
 
 document.getElementById("add").onclick = function () {
@@ -134,32 +102,36 @@ document.getElementById("add").onclick = function () {
 };
 
 document.querySelector("#ensemble-btn").addEventListener("click", function () {
-  const people = getPeople();
+  const startTime = getStartTime();
+  if (startTime) {
+    alert("Timer is already started.");
+    return;
+  }
 
+  const people = getPeople();
   if (people.length < 3) {
     alert("Need more people!\n Add at least 3 people to the list!");
   } else {
     alert("Ensemble started!");
-    iself.startTimer(10);
-    console.log(people);
-    const list = document
-      .getElementById("list_item")
-      .getElementsByTagName("li");
-    // document.getElementById("people_role").innerText = list[0].innerText + " " + list[1].innerText + " " + list[2].innerText
-    setText("roles-title", "Roles");
-    setText("role1", people[0] + " " + "is NAVIGATOR");
-    setText("role2", people[1] + " " + "is TYPIST");
-    setText("role3", people[2] + " " + "is CO-NAVIGATOR");
+    setStartTime(new Date());
+    renderEnsembleList();
   }
 });
 
 document.querySelector("#reset").addEventListener("click", function () {
-    removeAllPeople()
-    renderPeopleList()
-})
+  setStartTime(null);
+  removeAllPeople();
+  renderPeopleList();
+  clearEnsembleList();
+});
 
 function initialize() {
-  renderPeopleList()
+  renderPeopleList();
+  renderEnsembleList();
 }
+
+setInterval(() => {
+  document.querySelector("#time-display").textContent = getDisplayTime();
+}, 200);
 
 initialize();
